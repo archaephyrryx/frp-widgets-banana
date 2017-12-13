9222947ebede1ac10dd485d53bd8a2b5d7b038e0
Peter Duchovni <caufeminecraft@gmail.com>
2017-12-13 13:43:25 -0500

    added public export module to avoid conflict

---
+++ src/Widgets/Banana.hs
@@ -0,0 +1,39 @@
+module Widgets.Banana (
+  module Widgets.Banana.Cast,
+  module Widgets.Banana.Core,
+  module Widgets.Banana.Counter,
+  module Widgets.Banana.Fields,
+  module Widgets.Banana.Frame,
+  module Widgets.Banana.Group,
+  module Widgets.Banana.Input,
+  module Widgets.Banana.Links,
+  module Widgets.Banana.MonoSelect,
+  module Widgets.Banana.MultiSelect,
+  module Widgets.Banana.Obscura,
+  module Widgets.Banana.Phantom,
+  module Widgets.Banana.Radio,
+  module Widgets.Banana.Ranger,
+  module Widgets.Banana.Recorder,
+  module Widgets.Banana.Table,
+  module Widgets.Banana.Text,
+  module Widgets.Banana.Tower
+) where
+
+import qualified Widgets.Cast as Widgets.Banana.Cast
+import qualified Widgets.Core as Widgets.Banana.Core
+import qualified Widgets.Counter as Widgets.Banana.Counter
+import qualified Widgets.Fields as Widgets.Banana.Fields
+import qualified Widgets.Frame as Widgets.Banana.Frame
+import qualified Widgets.Group as Widgets.Banana.Group
+import qualified Widgets.Input as Widgets.Banana.Input
+import qualified Widgets.Links as Widgets.Banana.Links
+import qualified Widgets.MonoSelect as Widgets.Banana.MonoSelect
+import qualified Widgets.MultiSelect as Widgets.Banana.MultiSelect
+import qualified Widgets.Obscura as Widgets.Banana.Obscura
+import qualified Widgets.Phantom as Widgets.Banana.Phantom
+import qualified Widgets.Radio as Widgets.Banana.Radio
+import qualified Widgets.Ranger as Widgets.Banana.Ranger
+import qualified Widgets.Recorder as Widgets.Banana.Recorder
+import qualified Widgets.Table as Widgets.Banana.Table
+import qualified Widgets.Text as Widgets.Banana.Text
+import qualified Widgets.Tower as Widgets.Banana.Tower
